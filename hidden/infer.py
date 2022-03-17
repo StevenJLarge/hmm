72ba8e241e662adb29f79e3e0042c34b015f0206
Steven Large <slarge@sfu.ca>
2022-03-17 12:56:35 -0600

    Started working on building out inferrence functionality

--- hidden/infer.py
+++ hidden/infer.py
@@ -21,22 +21,43 @@ class MarkovInfer:
         self.bayes_filter = np.ones(self.n_sys) / self.n_sys
         self.forward_tracker = [self.bayes_filter]
 
+    def _initialize_back_tracker(self):
+        self.back_filter = self.bayes_filter[-1]
+        self.back_tracker = [0] * len(self.bayes_filter)
+        self.back_tracker[-1] = self.back_filter
+
     # Filtering and processing routines
-    def bayesian_filter(self, obs: int):
-        self.bayes_prob = np.matmul(self.A, self.bayes_prob)
-        self.bayes_filter = self.B[:, obs] * self.bayes_prob
+    def bayesian_filter(self, obs: int, A: np.ndarray, B: np.ndarray):
+        self.bayes_prob = np.matmul(A, self.bayes_prob)
+        self.bayes_filter = B[:, obs] * self.bayes_prob
         self.bayes_filter /= np.sum(self.bayes_filter)
 
-    def forward_algo(self, observations: Iterable[int]):
+    def bayesian_back(self, obs: int, A: np.ndarray, B: np.ndarray):
+        pass
+
+    def forward_algo(
+        self, observations: Iterable[int],
+        trans_matrix: np.ndarray,
+        obs_matrix: np.ndarray
+    ):
         # Runs the fwd algo over an entire sequence
         self._initialize_bayes_tracker()
 
         for obs in observations:
-            self.bayesian_filter(obs)
+            self.bayesian_filter(obs, trans_matrix, obs_matrix)
             self.bayes_tracker.append(self.bayes_filter)
 
-    def backward_algo(self, oservations: Iterable[int]):
-        pass
+    def backward_algo(
+        self,
+        observations: Iterable[int],
+        trans_matrix: np.ndarray,
+        obs_matrix: np.ndarray
+    ):
+        self._initialize_back_tracker()
+
+        for i, obs in enumerate(np.flip(observations)):
+            pass
+
 
     def bayesian_smooth(self):
         pass
