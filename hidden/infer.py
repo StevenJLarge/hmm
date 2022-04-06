118e5d80c18a26e1619d2a1b22f6bb945f7ce34d
Steven Large <slarge@sfu.ca>
2022-04-05 19:51:45 -0600

    Added code to the inference class

--- hidden/infer.py
+++ hidden/infer.py
@@ -23,8 +23,7 @@ class MarkovInfer:
 
     def _initialize_back_tracker(self):
         self.back_filter = self.bayes_filter[-1]
-        self.back_tracker = [0] * len(self.bayes_filter)
-        self.back_tracker[-1] = self.back_filter
+        self.back_tracker = [self.back_filter]
 
     # Filtering and processing routines
     def bayesian_filter(self, obs: int, A: np.ndarray, B: np.ndarray):
@@ -33,7 +32,9 @@ class MarkovInfer:
         self.bayes_filter /= np.sum(self.bayes_filter)
 
     def bayesian_back(self, obs: int, A: np.ndarray, B: np.ndarray):
-        pass
+        self.back_prob = np.matmul(A.T, self.back_prob)
+        self.back_filter = B[obs, :] * self.back_prob
+        self.back_filter /= np.sum(self.back_filter)
 
     def forward_algo(
         self, observations: Iterable[int],
@@ -55,21 +56,30 @@ class MarkovInfer:
     ):
         self._initialize_back_tracker()
 
-        for i, obs in enumerate(np.flip(observations)):
-            pass
+        for obs in enumerate(np.flip(observations)):
+            self.backward_filter(obs, trans_matrix, obs_matrix)
+            self.back_tracker.append(self.back_filter)
 
+    def bayesian_smooth(self, A: np.ndarray):
+        self.bayes_smoother = [[]] * len(self.forward_tracker)
+        self.bayes_smoother[-1] = np.array(self.forward_tracker[-1])
 
-    def bayesian_smooth(self):
-        pass
+        for i in range(len(self.forward_tracker) - 1):
+            prediction = np.matmul(A, self.forward_tracker[-(i + 2)])
+            summand = [np.sum(self.bayes_smoother[-(i+1)] * A[:, j] / prediction) for j in range(A.shape[1])]
+            self.bayes_smoother[-(i + 2)] = self.forward_tracker[-(i + 2)] * np.array(summand)
 
-    def forward_backward_algo(self):
-        pass
+    def discord(self, obs: Iterable) -> float:
+        if len(self.filter_tracker) - 1 != len(obs):
+            raise ValueError(
+                "You must run `forward_algo(...)` before `discord`..."
+            )
 
-    def discord(self):
-        pass
+        error = [1 if f == o else -1 for f, o in zip(self.filter_tracker, obs)]
+        return 1 - np.mean(error)
 
-    def error_rate(self):
-        pass
+    def error_rate(self, pred_ts: Iterable, state_ts: Iterable) -> float:
+        return np.sum([pred_ts == state_ts])/len(state_ts)
 
     # Inferrence routines
     def expectation(self):
