9a047b2faafd10de8b97bce59ca8ea8f5465851d
Steven Large <slarge@sfu.ca>
2023-03-31 22:31:50 -0600

    Finished first pass at modular optimization routines, still need to fix parameter extraction and encoding logic

--- hidden/optimize/optimization.py
+++ hidden/optimize/optimization.py
@@ -1,22 +1,23 @@
 # Routines for running optimizations in likelihood calcualtions
 from abc import ABC, abstractmethod
+from operator import mul
 import os
 from typing import Iterable, Tuple, Optional
 
 import numpy as np
 import numba
-import scipy.optimize
+import scipy.optimize as so
 
 from hidden.infer import MarkovInfer
-from hidden.optimize.results import LocalOptimizationResult
+from hidden.optimize.results import LocalOptimizationResult, GlobalOptimizationResult
 
 _MTX_ENCODING = "ROW_MAJOR"
 
 
 class BaseOptimizer(ABC):
-    def __init__(self, analyzer: MarkovInfer, mtx_encoding: Optional[str] = _MTX_ENCODING):
+    def __init__(self, mtx_encoding: Optional[str] = _MTX_ENCODING):
         self.status = 0
-        self.analyzer = analyzer
+        self.result = None
         self.bayes_filter = None
         self.predictions = None
         self.matrix_encoding = mtx_encoding
@@ -24,7 +25,49 @@ class BaseOptimizer(ABC):
     def __repr__(self):
         return f"{self.__name__}(status={self.status})"
 
-    def _extract_parameters(self, param_arr: Tuple) -> Tuple[np.ndarray, np.ndarray]:
+    @staticmethod
+    @numba.jit(nopython=True)
+    def _forward_algo(observations, trans_matrix, obs_matrix):
+        bayes_track = np.zeros((trans_matrix.shape[0], len(observations)))
+        pred_track = np.zeros((trans_matrix.shape[0], len(observations)))
+        bayes_ = np.ones(trans_matrix.shape[0]) / trans_matrix.shape[0]
+
+        for i, obs in enumerate(observations):
+            bayes_, pred_ = BaseOptimizer._bayesian_filter(obs, trans_matrix, obs_matrix, bayes_)
+            bayes_track[: i] = bayes_
+            pred_track[:, i] = pred_
+
+        return bayes_track, pred_track
+
+    @staticmethod
+    @numba.jit(nopython=True)
+    def _bayesian_filter(obs: int, A: np.ndarray, B: np.ndarray, bayes_: np.ndarray):
+        bayes_ = A @ bayes_
+        pred_ = bayes_.copy()
+        bayes_ = B[:, obs] * bayes_
+        bayes_ /= np.sum(bayes_)
+        return bayes_, pred_
+
+    @abstractmethod
+    def optimize(self):
+        pass
+
+
+class LikelihoodOptimizer(BaseOptimizer):
+
+    def _encode_parameters(self, A, B):
+        encoded = np.zeros(4 + mul(*A.shape) + mul(*B.shape))
+        encoded[: 2] = A.shape
+        encoded[2: 4] = B.shape
+        encoded[4: 4 + mul(*A.shape)] = np.ravel(A)
+        encoded[4 + mul(*A.shape):] = np.ravel(B)
+        return encoded
+
+    @staticmethod
+    @numba.jit(nopython=True)
+    def _extract_parameters(param_arr: Tuple) -> Tuple[np.ndarray, np.ndarray]:
+        # This is not totally true, need to set diagonal elements as sum to
+        # preserve cons of prob
         dimension_config = param_arr[:4]
         A_size = dimension_config[0] * dimension_config[1]
 
@@ -35,23 +78,6 @@ class BaseOptimizer(ABC):
         obs_mat = obs_mat.reshape(dimension_config[2], dimension_config[3])
         return trans_mat, obs_mat
 
-    def _forward_algo(self):
-        # Runs the fwd algo over an entire sequence
-        self._initialize_bayes_tracker()
-
-        if prediction_tracker:
-            self._initialize_pred_tracker()
-
-        for obs in observations:
-            self.bayesian_filter(obs, trans_matrix, obs_matrix, prediction_tracker)
-            self.forward_tracker.append(self.bayes_filter)
-
-    @abstractmethod
-    def optimize(self):
-        pass
-
-
-class LikelihoodOptimizer(BaseOptimizer):
     @staticmethod
     @numba.jit(nopython=True)
     def _likelihood(
@@ -65,42 +91,64 @@ class LikelihoodOptimizer(BaseOptimizer):
 
     @staticmethod
     @numba.jit(nopython=True)
-    def _forward_algo(
-        observations: Iterable[int], trans_matrix: np.ndarray,
-        obs_matrix: np.ndarray
-    ) -> np.ndarray:
-        pass
+    def calc_likelihood(param_arr: Iterable, obs_ts: Iterable) -> float:
+        A, B = LikelihoodOptimizer._extract_hmm_parameters(param_arr)
+        _, pred = BaseOptimizer._forward_algo(obs_ts, A, B)
+        return LikelihoodOptimizer._likelihood(pred, B, obs_ts)
 
-    # TODO make this into a static method
-    def _calc_likelihood(
-        self, param_arr: Iterable, obs_ts: Iterable, *args
-    ) -> float:
-        # NOTE Currently this only works for a 2D HMM
-        A, B = self._extract_hmm_parameters(param_arr)
-        # This populates the self.predictions array, which is used by the
-        # calc_likelihood below
-        self.forward_algo(obs_ts, A, B, prediction_tracker=True)
-        return self.calc_likelihood(B, obs_ts)
+    def _build_optimization_bounds(self, n_params: int, lower_lim: Optional[float] = 1e-3, upper_lim: Optional[float] = 1 - 1e-3) -> Iterable:
+        return [(lower_lim, upper_lim)] * n_params
 
 
-class LocalLikelihoodOptimizer(BaseOptimizer):
+class LocalLikelihoodOptimizer(LikelihoodOptimizer):
     def __init__(
         self, analyzer: MarkovInfer, algorithm: Optional[str] = "Nelder-Mead"
-    ):
-        self.algo
-        super().__init__(analyzer)
-
-    def optimize(self) -> LocalOptimizationResult:
-        pass
-
-
-class GlobalLikelihoodOptimizer(BaseOptimizer):
-    def __init__(
-        self, analyzer: MarkovInfer, algorithm: Optional[str] = "sobol"
     ):
         self.algo = algorithm
         super().__init__(analyzer)
 
-    def optimize(self):
-        pass
+    def optimize(self, obs_ts, A_guess, B_guess) -> LocalOptimizationResult:
+        param_init = self._encode_parameters(A_guess, B_guess)
+        opt_args = (obs_ts,)
+        bnds = self._build_optimization_bounds(param_init)
 
+        self.result = so.minimize(
+            fun=LikelihoodOptimizer.calc_likelihood,
+            x0=param_init,
+            args=opt_args,
+            method=self.algo,
+            bounds=bnds
+        )
+
+        return LocalOptimizationResult(self)
+
+
+class GlobalLikelihoodOptimizer(LikelihoodOptimizer):
+    def __init__(
+        self, analyzer: MarkovInfer, algorithm: Optional[str] = "sobol"
+    ):
+        self.sampling_algo = algorithm
+        super().__init__(analyzer)
+
+    def optimize(self, obs_ts, *args):
+        opt_args = (obs_ts,)
+        # NOTE update this to pass in an actual parameter number
+        bnds = self._build_optimization_bounds()
+
+        self.result = so.shgo(
+            fun=LikelihoodOptimizer.calc_likelihood,
+            bounds=bnds,
+            args=opt_args,
+            sampling_method=self.sampling_algo
+        )
+
+        return GlobalOptimizationResult(self)
+
+
+class EMOptimizer(BaseOptimizer):
+    pass
+
+
+if __name__ == "__main__":
+    # testing routines here
+    pass
