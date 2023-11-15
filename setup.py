38127df851056cda3309be7e2cad3f67b87193a3
Steven Large <slarge@viewpointgroup.ca>
2023-11-15 13:20:43 -0700

    ticked minor version, as property refactor will introduce a breaking change

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 setup(
     name='hidden-py',
     packages=find_packages(),
-    version="1.0.20",
+    version="1.1.0",
     long_description=long_description,
     long_description_content_type='text/markdown',
     description='''
